f94b343be736f568fb2525ce84d1c82c97e37506
Jason Khor <kzsheng_1996@hotmail.com>
2018-02-02 12:35:03 +0800

    Reset everything.
    I restored everything back to its original form.

--- Sales and Inventory System/frmLogin.vb
+++ Sales and Inventory System/frmLogin.vb
@@ -5,6 +5,22 @@ Public Class frmLogin
         End
     End Sub
 
+    ' TODO: Insert code to perform custom authentication using the provided username and password 
+    ' (See http://go.microsoft.com/fwlink/?LinkId=35339).  
+    ' The custom principal can then be attached to the current thread's principal as follows: 
+    '     My.User.CurrentPrincipal = CustomPrincipal
+    ' where CustomPrincipal is the IPrincipal implementation used to perform authentication. 
+    ' Subsequently, My.User will return identity information encapsulated in the CustomPrincipal object
+    ' such as the username, display name, etc.
+
+
+
+
+
+
+
+
+
     Private Sub frmLogin_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
     End Sub
